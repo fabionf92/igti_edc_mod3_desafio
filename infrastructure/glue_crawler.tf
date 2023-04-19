ebb6747b4a365c81002c3811078d98b9f1cd6088
Franco Fábio N <FrancoFabioN@JohnDeere.com>
2023-04-19 14:02:23 -0300

    role adjusted

--- infrastructure/glue_crawler.tf
+++ infrastructure/glue_crawler.tf
@@ -3,7 +3,7 @@
 resource "aws_glue_crawler" "processed_data_crawler" {
   name          = "processed-data-crawler"
   role          = aws_iam_role.glue_role.arn
-  database_name = "edc_mod2_desafio_database"
+  database_name = "edc_mod3_desafio_database"
 
   s3_target {
     path = "s3://${aws_s3_bucket.datalake.bucket}/staging-zone/"
